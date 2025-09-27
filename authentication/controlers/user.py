ea637f3db79cb9a1826d9e768c623042953993e8
rupak26 <rupakbiswas29bd@gmail.com>
2025-09-27 17:17:48 +0600

    Introsecpect in comment

--- authentication/controlers/user.py
+++ authentication/controlers/user.py
@@ -44,3 +44,49 @@ def show_user(db : Session = Depends(get_db)):
 def show_user(id:int , db : Session = Depends(get_db)):
     return user.show_user_by_id(id , db)
 
+########################################################
+# @router.post(
+#     "/introspect" ,
+#     summary="Give token to different services",
+#     include_in_schema=False
+#     )
+# def introspect_imple(request: TokenRequest):
+#     service = UserServicesImpl()
+#     response = service.introspect_token(request)
+#     return response
+
+# def introspect_token(self, request):
+#         try:
+#             db_row = user_repo.introspect_tokens(self,request.token)
+#             payload = jwt.decode(request.token, SECRET_KEY, algorithms=[ALGORITHM])
+
+#             if db_row:
+#                 return {
+#                     "active": True,
+#                     "sub": payload.get("sub"),
+#                     "role": payload.get("role"),
+#                     "exp": payload.get("exp")
+#                 }
+#             else:
+#                 return {"active": False}
+#         except JWTError as e:
+#             logger.error(f"JWT error in introspect: {str(e)}")
+#             return {"active": False, "error": "Invalid token"}
+#         except Exception as e:
+#             logger.error(f"Unexpected error in introspect: {str(e)}")
+#             return {"active": False, "error": str(e)}
+        
+# def introspect_tokens(self, token: str):
+#     try:
+#         with pool.acquire() as conn:
+#             cursor = conn.cursor()
+#             cursor.execute(
+#                 """SELECT * FROM AUTH_TOKEN WHERE ACCESS_TOKEN = :token FETCH FIRST 1 ROWS ONLY""",
+#                 {"token": token}
+#             )
+#             return cursor.fetchone()
+#     except oracledb.DatabaseError as e:
+#         logger.error(f"DB error in introspect_token: {e.args[0].message}")
+#         raise
+
+###############################################################
